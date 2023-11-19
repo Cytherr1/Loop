feeb9d5ff06e7ffa7fd57942f58ad403f075566c
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2023-11-19 13:39:49 +0300

    added sheets

--- MagaraJam6/Assets/Scripts/Respawn.cs
+++ MagaraJam6/Assets/Scripts/Respawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Respawn : MonoBehaviour
@@ -7,10 +8,27 @@ public class Respawn : MonoBehaviour
     [SerializeField] AudioClip respawn;
     [SerializeField] GameObject spawn;
     [SerializeField] GameObject particle;
+    [SerializeField] GameObject ucantdie;
+    private float sec = 1.5f;
+    private float timer = 0;
+
+    private void Update()
+    {
+        if (ucantdie.activeSelf == true)
+        {
+            timer += Time.deltaTime;
+            if (timer == sec) 
+            {
+                ucantdie.SetActive(false);
+                timer = 0;
+            }
+        }
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject.FindWithTag("Player").transform.position = spawn.transform.position;
         Instantiate(particle, spawn.transform.position, Quaternion.identity);
         GameObject.FindWithTag("Player").GetComponent<AudioSource>().PlayOneShot(respawn);
+        ucantdie.SetActive(true);
     }
 }
