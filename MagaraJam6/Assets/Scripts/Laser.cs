520c608867fc0ec5eef3a95d5dbbed8c3b4c582f
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2023-11-19 16:21:05 +0300

    laser fix

--- MagaraJam6/Assets/Scripts/Laser.cs
+++ MagaraJam6/Assets/Scripts/Laser.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] AudioClip respawn;
+    [SerializeField] GameObject spawn;
     public LineRenderer lineRenderer;
     public Transform laserPosition;
     public Vector2 vec;
@@ -14,12 +16,12 @@ public class Laser : MonoBehaviour
         lineRenderer.SetPosition(0, laserPosition.position);
         if (hit)
         {
-
-
-
             lineRenderer.SetPosition(1, hit.point);
-
-
+        }
+        if (hit.transform.gameObject.CompareTag("Player"))
+        {
+            GameObject.FindWithTag("Player").transform.position = spawn.transform.position;
+            GameObject.FindWithTag("Player").GetComponent<AudioSource>().PlayOneShot(respawn);
         }
 
     }
